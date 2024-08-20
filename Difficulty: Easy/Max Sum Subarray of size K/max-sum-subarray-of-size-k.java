57b77bbacec9914affabd7dba393daf41ebf45dc
coder-Bhattji-210102 <gamingmb277@gmail.com>
2024-08-20 13:32:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N=sc.nextInt();
+            int K=sc.nextInt();
+            ArrayList<Integer> Arr = new ArrayList<Integer>(N); 
+  
+            // Appending new elements at 
+            // the end of the list 
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+                Arr.add(x); 
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.maximumSumSubarray(K,Arr,N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+        // code here
+        int start=0;
+        long sum=0;
+        long maxi=Integer.MIN_VALUE;
+        
+        for(int end=0;end<N;end++){
+            sum+=Arr.get(end);
+            
+            if(end-start+1==K){
+                maxi=Math.max(sum,maxi);
+                sum-=Arr.get(start);
+                start++;
+            }
+        }
+        return maxi;
+        
+    }
+}
